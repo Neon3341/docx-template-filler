d95dc483d8b4518dd811621d631d0296a90b9272
Neon3341 <et0105@mail.ru>
2024-09-21 14:53:32 +0300

    input change

--- src/renderer/src/pages/components/FCKinput.jsx
+++ src/renderer/src/pages/components/FCKinput.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react'
 import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
 
-const CssTextField = styled(TextField)({
-    '& label.Mui-focused': {
-        color: '#fff',
-    },
-    '& .MuiInput-underline:after': {
-        borderBottomColor: '#B2BAC2',
-    },
-    '& .MuiOutlinedInput-root': {
-        '& fieldset': {
-            borderColor: '#E0E3E7',
-        },
-        '&:hover fieldset': {
-            borderColor: '#B2BAC2',
-        },
-        '&.Mui-focused fieldset': {
-            borderColor: '#fff',
-        },
-    },
-});
+
 const BackInput = ({name, label, value, onChange }) => {
 
     return (
-        <CssTextField
+        <TextField
                 label={label}
                 type="text"
                 name={name}
